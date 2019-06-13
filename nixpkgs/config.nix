1592e33cde277a86055d7fb74494b170996e956f
Lukas Einhaus <lukas.einhaus@stud.uni-due.de>
2019-06-13 12:50:05 +0200

    added bazel and clion packages

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -2,6 +2,6 @@
   firefox.enableGnomeExtesions = true;
   allowUnfree = true;
   packageOverrides = pkgs: with pkgs; {
-    pythonEnv = python36.withPackages (ps: with ps; [setuptools numpy matplotlib]);
+    pythonEnv = python37.withPackages (ps: with ps; [setuptools pydbus dbus-python numpy matplotlib pip pygobject3 scipy breathe sphinx_rtd_theme doc8]);
     };
 }
--- nixpkgs/overlays/development.nix
+++ nixpkgs/overlays/development.nix
@@ -1,11 +1,30 @@
-let unstable = import <unstable> {overlays = [];}; in
+let
+	unstable = import <unstable> {overlays = [];}; 
+in
 self: super: {
 	developmentEnv = self.buildEnv {
 		name = "developmentEnv";
 		paths = with self.pkgs; [
-			unstable.bazel
+                        unstable.buildifier
 			nodejs
 			unstable.spaceship-prompt
+			cquery
+			unstable.rustc
+			unstable.cargo
+			doxygen
+			texlive.combined.scheme-medium
 		];
 	};
+#	avrgcc-wrapper = self.pkgs.stdenv.mkDerivation rec {
+#	  name = "avrgcc-wrapper-${version}";
+#	  version = self.pkgs.stdenv.lib.strings.getVersion self.pkgs.avrgcc.name;
+#	  nativeBuildInputs = [ self.pkgs.makeWrapper ];
+#	  buildCommand = ''
+#	    mkdir -p $out/bin
+#	    for exe in gcc g++; do
+#	      makeWrapper "${self.pkgs.avrgcc}/bin/avr-$exe" "$out/bin/avr-$exe" --add-flags "-B${self.pkgs.avrlibc}/avr/lib -isystem ${self.pkgs.avrlibc}/avr/include"
+#	    done
+#	    ln -s ${self.pkgs.avrgcc}/lib $out
+#	  '';
+#};
 } 
